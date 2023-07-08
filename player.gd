c409261fd46ccd8b344e44649112cb75596dcf7c
Caspar Krieger <git@asparck.com>
2023-07-08 20:54:37 +0800

    Add coyote jump

--- player.gd
+++ player.gd
@@ -9,6 +9,7 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
 
 func _physics_process(delta: float):
     apply_gravity(delta)
@@ -21,17 +22,20 @@ func _physics_process(delta: float):
     handle_acceleration(direction, delta)
     handle_animation(direction)
 
+    var was_on_floor = is_on_floor()
     move_and_slide()
+    if was_on_floor and not is_on_floor() and velocity.y >= 0:
+        coyote_timer.start()
 
 func apply_gravity(delta: float):
     if not is_on_floor():
         velocity.y += gravity * delta
 
 func handle_jump():
-    if is_on_floor():
+    if is_on_floor() or not coyote_timer.is_stopped():
         if Input.is_action_just_pressed("ui_accept"):
             velocity.y = JUMP_VELOCITY
-    else:
+    if not is_on_floor():
         if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 3:
             velocity.y = JUMP_VELOCITY / 3
 
