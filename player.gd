31e627de84f228043c96d47cc7f1fad2e9a64639
Caspar Krieger <git@asparck.com>
2023-07-09 20:36:47 +0800

    Add air jump

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var movement_data: PlayerMovementData
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+var air_jump = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
@@ -18,6 +18,7 @@ func _physics_process(delta: float):
     apply_friction(input_axis, delta)
     apply_air_resistance(input_axis, delta)
     handle_acceleration(input_axis, delta)
+    handle_air_acceleration(input_axis, delta)
     handle_animation(input_axis)
 
     var was_on_floor = is_on_floor()
@@ -32,6 +33,9 @@ func apply_gravity(delta: float):
         velocity.y += gravity * movement_data.gravity_scale * delta
 
 func handle_jump():
+    if is_on_floor():
+        air_jump = true
+
     if is_on_floor() or not coyote_timer.is_stopped():
         if Input.is_action_just_pressed("ui_up"):
             velocity.y = movement_data.jump_velocity
@@ -39,9 +43,21 @@ func handle_jump():
         if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 3:
             velocity.y = movement_data.jump_velocity / 3
 
+        if Input.is_action_just_pressed("ui_up") and air_jump:
+            velocity.y = movement_data.jump_velocity / 2
+            air_jump = false
+
 func handle_acceleration(input_axis: float, delta: float):
+    if not is_on_floor():
+        return
     if input_axis:
         velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis: float, delta: float):
+    if is_on_floor():
+        return
+    if input_axis:
+        velocity.x = move_toward(velocity.x, input_axis * movement_data.speed, movement_data.air_acceleration * delta)
 
 func apply_friction(input_axis: float, delta: float):
     if input_axis == 0 and is_on_floor():
